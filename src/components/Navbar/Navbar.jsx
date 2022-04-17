7de1cf2202d9e035e14f6a65b5ddb2f04cf5a8e2
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-04-17 09:41:56 +0530

    feat: add conditional rendering to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import "./navbar.css";
 import {logo} from "../../assets";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function Navbar(){
+    const {pathname} = useLocation();
 
     return (
         <header className="header">
             <div className="logo-wrapper">
                 <Link to="/home"><img src={logo} alt="vacay-stream-logo" className="logo" /></Link>
             </div>
+            {pathname==="/home" && 
             <div className="search-box">
                 <i className="fas fa-search"></i>
                 <input type="text" placeholder="Search" />
-            </div>
+            </div>}
             <button title="Dark Mode" className="btn-icon"><i className="fas fa-moon"></i></button>
         </header>
     );
